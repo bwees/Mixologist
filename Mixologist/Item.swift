da971a4a7c973f267dcd21afa880d9c47f98f735
bwees <brandonwees@gmail.com>
2024-01-30 12:20:20 -0600

    Initial Commit

---
+++ Mixologist/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Mixologist
+//
+//  Created by Brandon Wees on 1/30/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
